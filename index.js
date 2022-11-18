aa2892f0d19c5b5765a84814b99ce2873844ad6a
Vaikehu <118215415+Vaikehu@users.noreply.github.com>
2022-11-18 13:45:24 -1000

    2 files modified

--- index.js
+++ index.js
@@ -1,37 +1,62 @@
-
-const card = `
-  <div class="col-4 col-md-4 col-sm-12">
+const card = (title, description, image) => `
+  <div class="col-lg-4 col-sm-12">
     <div class="card mb-5 shadow-sm">
-      <img src="https://picsum.photos/1000/600" class="img-fluid" />
+      <img src= ${image} class="img-fluid" />
 
       <div class="card-body">
         <div class="card-title">
-          <h2>This is the post title</h2>
+          <h2>${title}</h2>
         </div>
         <div class="card-text">
           <p>
-            En effet, mon profil correspond à la description recherchée sur
-            l’offre d’emploi (préciser où l’annonce a été vue). (Si le candidat
-            possède peu d’expérience professionnelle) Ma formation en (préciser la
-            formation) m'a permis d'acquérir de nombreuses compétences parmi
-            celles que vous recherchez. Je possède tous les atouts qui me
-            permettront de réussir dans le rôle que vous voudrez bien me confier.
-            Motivation, rigueur et écoute sont les maîtres mots de mon
-            comportement professionnel...
+            ${description}
           </p>
         </div>
         <a href="#" class="btn btn-outline-info rounded-0 float-end">Read More</a>
+        </div>
       </div>
     </div>
   </div>
 `;
 
 (async () => {
-  const apiData = await fetch('https://jsonplaceholder.typicode.com/posts').then(
-    (response) => response.json()
-  );
-  console.log(apiData)
-  const dataDiv = document.getElementById('data');
-  dataDiv.innerHTML = card;
+  const apiData = await fetch(
+    'https://jsonplaceholder.typicode.com/posts'
+  ).then((response) => response.json());
 
+  const myData = [
+    {
+      title: 'LUNDI',
+      description:
+        'UNIVER DU WEB <br> Découvert du deuxieme module avec notre Formation FABRICE, puis initiation du navigateur, des langage HTML CSS JAVASCRIP... <br>  ',
+      image: 'https://picsum.photos/seed/picsum/200/300',
+    },
+    {
+      title: 'MARDI',
+      description: 'HTML_CSS_JAVASCRIPTE',
+      image: 'https://picsum.photos/200/300?grayscale',
+    },
+    {
+      title: 'MERCREDI',
+      description: 'DISGNE_CSS_TEMPLATE',
+      image: 'https://picsum.photos/seed/picsum/200/300',
+    },
+    {
+      title: 'JEUDI',
+      description: 'BOOTSTRAP_PORTFOLIO_BLOG <br>',
+      image: 'https://picsum.photos/200/300?grayscale',
+    },
+    {
+      title: 'VENDREDI',
+      description: 'FINISH_DU_BLOG',
+      image: 'https://picsum.photos/seed/picsum/200/300',
+    },
+  ];
+  myData.forEach((post) => {
+    document.getElementById((id = 'card-list')).innerHTML += card(
+      post.title,
+      post.description,
+      post.image
+    );
+  });
 })();
